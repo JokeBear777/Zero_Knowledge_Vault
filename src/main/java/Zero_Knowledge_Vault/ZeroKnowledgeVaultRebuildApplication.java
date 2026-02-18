3e6b4983cd1e6ab154589de13e106be34933c428
Jokebear777 <korae9955@naver.com>
2026-02-18 11:37:51 +0900

    init

---
+++ src/main/java/Zero_Knowledge_Vault/ZeroKnowledgeVaultRebuildApplication.java
@@ -0,0 +1,13 @@
+package Zero_Knowledge_Vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroKnowledgeVaultRebuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroKnowledgeVaultRebuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Zero_Knowledge_Vault/ZeroKnowledgeVaultRebuildApplicationTests.java
@@ -0,0 +1,13 @@
+package Zero_Knowledge_Vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroKnowledgeVaultRebuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
